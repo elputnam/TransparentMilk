38457cd1d31a9d4771febb332ca411a7463b3807
fleshcircuit <elputnam@inaction.ie>
2021-09-03 17:06:08 +0100

    updated

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
 //Title generator
+var adj;
+var noun;
+var adjList = [];
+var nounList = [];
+let i = 0;
+let j = 0;
+
+function preload(){
+  adjList = loadStrings('words1.txt');
+  nounList = loadStrings('words2.txt');
+}
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  i = int(random(adjList.length));
+  j = int(random(nounList.length));
 }
 
 function draw() {
-  background(220);
+  background(200);
+  textAlign(CENTER)
+  textSize(width*.1)
+  text(adjList[i], width/2, height/2 - 50)
+  text(nounList[j], width/2, height/2 + 50)
 }
